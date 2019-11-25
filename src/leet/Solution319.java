232111291f44e8c39d64170fdb828554fb99e951
cuize2018 <847290082@qq.com>
2019-11-25 12:39:54 +0800

    灯泡开关问题

---
+++ src/leet/Solution319.java
@@ -0,0 +1,11 @@
+package leet;
+
+public class Solution319 {
+    public static void main(String[] args) {
+        System.out.println(bulbSwitch(1));
+    }
+
+    public static int bulbSwitch(int n) {
+        return 1;
+    }
+}
