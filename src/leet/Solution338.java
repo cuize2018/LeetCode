c61e0e793fa37af89bdfc57a44322afd808da7e3
cuize2018 <847290082@qq.com>
2019-11-29 14:29:59 +0800

    整数拆分

--- src/leet/Solution338.java
+++ src/leet/Solution338.java
@@ -8,6 +8,20 @@ public class Solution338 {
         System.out.println(Arrays.toString(countBits(num)));
     }
 
+    /**
+     * 规律如下：
+     * 若n为奇数 -> dp[n] = dp[n-1] + 1
+     * 若n为偶数 -> 若n为2的k次幂， 则dp[n] = 1
+     *          -> 否则, dp[n] = dp[2^(k-1)] + dp[n-2^(k-1)]
+     * exp: n = 12, 初始 k = 1
+     * 当i = 8时, k = 3, dp[8] = 1, k = k+1 = 4
+     * 当i = 12时, k = 4, dp[12] = dp[2^3] + dp[12-2^3] -> dp[12] = dp[8] + dp[4]
+     * 12: 1100 -> 1000 + 0100 -> 8 + 4
+     *
+     *
+     * @param num
+     * @return
+     */
     public static int[] countBits(int num) {
         int[] dp = new int[num+1];
         dp[0] = 0;
