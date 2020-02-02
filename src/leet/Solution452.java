543b29e53336a22e33a6ceaede44119c0b6b8a3b
cuize2018 <847290082@qq.com>
2020-02-02 17:31:21 +0800

    用最少数量的箭引爆气球

---
+++ src/leet/Solution452.java
@@ -0,0 +1,54 @@
+package leet;
+
+import java.util.*;
+
+public class Solution452 {
+    public static void main(String[] args) {
+        int[][] a = {{10,16}, {2,8}, {1,6}, {7,12}};
+        int[][] b = {{1,2}, {3,4}, {5,6}, {7,8}};
+        int[][] e = {{1,2}, {2,3}, {3,4}, {4,5}};
+        int[][] c = {{2,3}, {2,3}};
+        int[][] d = {{7,15},{6,14},{8,12},{3,4},{4,13},{6,14},{9,11},{6,12},{4,13}};
+        System.out.println(findMinArrowShots2(e));
+    }
+
+    /**
+     * 让我们根据气球的结束坐标进行排序，然后一个个进行检查。第一个气球是标有 0 的绿色气球，它的结束坐标是 6。
+     *
+     * 其他的气球有两种情况：
+     *     开始坐标小于 6，例如红色气球，它可以与 0 气球一起被一支箭引爆。
+     *     开始坐标大于 6，例如黄色气球，它不可以与 0 气球被一支箭引爆，因此需要增加箭的数量。
+     *
+     * 这代表了我们可以跟踪气球的结束坐标，若下个气球开始坐标在当前气球的结束坐标前，则我们可以用一支箭一起引爆；
+     * 若下个气球的开始坐标在当前气球的结束坐标后，则我们必须增加箭的数量。并跟踪下个气球的结束坐标。
+     *
+     * @param points
+     * @return
+     */
+    public static int findMinArrowShots2(int[][] points) {
+        if (points.length == 0) return 0;
+
+        // sort by x_end
+        Arrays.sort(points, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        int arrows = 1;
+        int xStart, xEnd, firstEnd = points[0][1];
+        for (int[] p : points) {
+            xStart = p[0];
+            xEnd = p[1];
+            // if the current balloon starts after the end of another one,
+            // one needs one more arrow
+            if (firstEnd < xStart) {
+                arrows++;
+                firstEnd = xEnd;
+            }
+        }
+
+        return arrows;
+    }
+}
