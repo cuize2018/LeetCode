62f6224806b7da97cf943459915b32baed3af4b0
cuize2018 <847290082@qq.com>
2020-02-07 21:40:41 +0800

    一和零

--- src/leet/Solution474.java
+++ src/leet/Solution474.java
@@ -160,6 +160,30 @@ public class Solution474 {
         return dp[m][n];
     }
 
+    public int findMaxForm3(String[] strs, int m, int n) {
+        int len = strs.length;
+        int[][][] dp = new int[len + 1][m + 1][n + 1];
+
+        for (int i = 1; i <= len; i++) {
+            // 注意：有一位偏移
+            int[] cnt = cal_info(strs[i - 1]);
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= n; k++) {
+                    // 先把上一行抄下来
+                    dp[i][j][k] = dp[i - 1][j][k];
+
+                    int zeros = cnt[0];
+                    int ones = cnt[1];
+
+                    if (j >= zeros && k >= ones) {
+                        dp[i][j][k] = Math.max(dp[i - 1][j][k], dp[i - 1][j - zeros][k - ones] + 1);
+                    }
+                }
+            }
+        }
+        return dp[len][m][n];
+    }
+
     public static int[] cal_info(String str){
         int[] tmp = new int[2];
         for (char c : str.toCharArray()){
