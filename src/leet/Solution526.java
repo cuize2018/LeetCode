25a6e07a4e7408337f4f1c2b333f7092ff57468a
cuize2018 <847290082@qq.com>
2020-02-23 10:48:47 +0800

    优美的排列

---
+++ src/leet/Solution526.java
@@ -0,0 +1,46 @@
+package leet;
+
+import java.util.*;
+
+public class Solution526 {
+//    List<Integer> list = new ArrayList<>();
+    int count = 0;
+    LinkedList<Integer> set = new LinkedList<>();
+
+    public static void main(String[] args) {
+        int N = 14;
+        Solution526 sol = new Solution526();
+        System.out.println(sol.countArrangement(N));
+    }
+
+    public int countArrangement(int N) {
+        for (int i = 1;i<=N;i++){
+            set.add(i);
+        }
+
+        helper(N, 1);
+        return count;
+    }
+
+
+    public void helper(int N, int index){
+        if (set.isEmpty()){
+            count++;
+            return;
+        }
+
+        int k = 0;
+        while (k < set.size()){
+            int val = set.get(k);
+            if (index % val == 0 || val % index == 0){
+//                list.add(val);
+                set.remove(k);
+                helper(N, index+1);
+
+                set.add(k, val);
+//                list.remove(list.size()-1);
+            }
+            k++;
+        }
+    }
+}
