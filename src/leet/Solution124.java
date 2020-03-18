06251472758e80d97f00698e58b4b90e5ae9afa5
cuize2018 <847290082@qq.com>
2020-03-18 22:38:57 +0800

    二叉树中的最大路径和

--- src/leet/Solution124.java
+++ src/leet/Solution124.java
@@ -21,11 +21,12 @@ public class Solution124 {
 
         //新路径的代价值，node节点为最高节点
         int price_new = node.val + left + right;
+        //旧路径的代价值，仍需使用node的父节点
+        int price_old = node.val + Math.max(left, right);
 
         //判断是否使用新路径，若新路径值大则选择新路径
-        max = Math.max(max, price_new);
-
-        return node.val + Math.max(left, right);
+        max = Math.max(max, Math.max(price_new,price_old));
+        return price_old;
     }
 
 
