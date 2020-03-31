c885025144ef92f97df628c827abde480a4d40ff
cuize2018 <847290082@qq.com>
2020-03-31 10:04:01 +0800

    排序数组

--- src/leet/Solution912.java
+++ src/leet/Solution912.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Solution912 {
     public static void main(String[] args) {
-        int[] nums = {5,1,1,2,0,0};
+        int[] nums = {5, 1, 1, 2, 0, 0};
         System.out.println(Arrays.toString(sortArray(nums)));
     }
 
@@ -17,12 +17,13 @@ public class Solution912 {
 
     /**
      * 快速排序
+     *
      * @param nums
      * @param low
      * @param high
      */
     private static void quickSort(int[] nums, int low, int high) {
-        if (low >= high)return;
+        if (low >= high) return;
 
         int m = partition(nums, low, high);
         quickSort(nums, low, m - 1);
@@ -57,5 +58,38 @@ public class Solution912 {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+
+    public static int[] sortArray2(int[] nums) {
+        if (nums.length <= 1) return nums;
+        quickSort2(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private static void quickSort2(int[] nums, int low, int high) {
+        if (low >= high) return;
+        int m = partition2(nums, low, high);
+        quickSort2(nums, low, m - 1);
+        quickSort2(nums, m + 1, high);
+    }
+
+    private static int partition2(int[] nums, int low, int high) {
+        int i = low;
+        int j = high + 1;
+        int v = nums[low];
+
+        while (true) {
+            while (nums[++i] < v) {
+                if (i == high) break;
+            }
+            while (nums[--j] > v) {
+                if (j == low) break;
+            }
+            if (i >= j) break;
+            swap(nums, i, j);
+        }
+        swap(nums, low, j);
+        return j;
     }
 }
