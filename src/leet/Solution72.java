8acf917660a4a2c24bd499c0ce2357792dea3b16
cuize2018 <847290082@qq.com>
2020-04-06 09:23:39 +0800

    编辑距离

--- src/leet/Solution72.java
+++ src/leet/Solution72.java
@@ -7,18 +7,19 @@ public class Solution72 {
     public static void main(String[] args) {
         String a = "horse";
         String ros = "ros";
-        System.out.println(minDistance(a,ros));
+        System.out.println(minDistance2(a, ros));
     }
 
     /**
      * dp[i][j] 代表 word1 到 i 位置转换成 word2 到 j 位置需要最少步数
+     *
      * @param word1
      * @param word2
      * @return
      */
     public static int minDistance(String word1, String word2) {
-        if (word1.length() == 0 || word2.length() == 0){
-            return word1.length()+word2.length();
+        if (word1.length() == 0 || word2.length() == 0) {
+            return word1.length() + word2.length();
         }
         //dp[i][j]代表word1的前i个字符变为word2的前j个字符的最短距离
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
@@ -39,14 +40,39 @@ public class Solution72 {
         //      反之，如果不同就替换最后一位（d[i-1][j-1]+1）
         for (int i = 1; i < dp.length; i++) {
             for (int j = 1; j < dp[0].length; j++) {
-                if (word1.charAt(i-1) == word2.charAt(j-1)){
-                    dp[i][j] = Math.min(Math.min(dp[i-1][j]+1, dp[i][j-1]+1), dp[i-1][j-1]);
-                }
-                else {
-                    dp[i][j] = Math.min(Math.min(dp[i-1][j]+1, dp[i][j-1]+1), dp[i-1][j-1]+1);
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1]);
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + 1);
                 }
             }
         }
-        return dp[dp.length-1][dp[0].length-1];
+        return dp[dp.length - 1][dp[0].length - 1];
     }
+
+    public static int minDistance2(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j < dp[0].length; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+                }
+            }
+        }
+
+        return dp[word1.length()][word2.length()];
+    }
+
+
 }
