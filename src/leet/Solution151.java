b2ff8ba2e0b2c06d4668ef348c813ef6468f8a3e
cuize2018 <847290082@qq.com>
2020-04-10 09:43:41 +0800

    翻转字符串里的单词

--- src/leet/Solution151.java
+++ src/leet/Solution151.java
@@ -1,14 +1,13 @@
 package leet;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import netscape.security.UserTarget;
+
+import java.util.*;
 
 public class Solution151 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String s = "the sky is blue";
-        System.out.println(reverseWords(s));
+        System.out.println(reverseWords2(s));
 
     }
 
@@ -17,19 +16,43 @@ public class Solution151 {
         List<String> words_new = new ArrayList<>();
         StringBuilder out = new StringBuilder();
 
-        for (int i = words.length-1;i>=0;i--){
+        for (int i = words.length - 1; i >= 0; i--) {
             if (!words[i].equals("")) {
                 words_new.add(words[i]);
             }
         }
 
-        for (int i = 0;i<words_new.size();i++){
+        for (int i = 0; i < words_new.size(); i++) {
             out.append(words_new.get(i));
-            if (i != words_new.size()-1){
+            if (i != words_new.size() - 1) {
                 out.append(" ");
             }
         }
 
         return out.toString();
+    }
+
+    public static String reverseWords2(String s) {
+        if (s.length() == 0) return "";
+        Stack<String> out = new Stack<>();
+        StringBuilder str = new StringBuilder();
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) != ' ') {
+                StringBuilder word = new StringBuilder();
+                while (i < s.length() && s.charAt(i) != ' ') {
+                    word.append(s.charAt(i));
+                    i++;
+                }
+                out.push(word.toString());
+                out.push(" ");
+            }
+            i++;
+        }
+        if (!out.isEmpty()) out.pop();
+        while (!out.isEmpty()) {
+            str.append(out.pop());
+        }
+        return str.toString();
     }
 }
