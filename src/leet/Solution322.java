f622c3a5c4d475d9566b738810f5dbd93a25242a
cuize2018 <847290082@qq.com>
2020-04-23 10:49:38 +0800

    零钱兑换

--- src/leet/Solution322.java
+++ src/leet/Solution322.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Solution322 {
     public static void main(String[] args) {
         int[] a = {186, 419, 83, 408};
-        int amount = 6249;
-        System.out.println(coinChange(a, amount));
+        int amount = 0;
+        System.out.println(coinChange3(a, amount));
     }
 
     public static int coinChange(int[] coins, int amount) {
@@ -32,24 +32,40 @@ public class Solution322 {
 
 
     public static int coinChange2(int[] coins, int amount) {
-        int[] dp = new int[amount+1];
+        int[] dp = new int[amount + 1];
         dp[0] = 0;
 
-        for (int amt = 1;amt <= amount;amt++){
+        for (int amt = 1; amt <= amount; amt++) {
             int min = Integer.MAX_VALUE;
-            for (int i = 0;i<coins.length;i++){
+            for (int i = 0; i < coins.length; i++) {
                 if (amt > coins[i]) {
-                    int tmp =  dp[amt - coins[i]];
-                    if (tmp != -1){
+                    int tmp = dp[amt - coins[i]];
+                    if (tmp != -1) {
                         min = Math.min(min, tmp + 1);
                     }
                 }
             }
-            if (min == Integer.MAX_VALUE)dp[amt] = -1;
+            if (min == Integer.MAX_VALUE) dp[amt] = -1;
             else dp[amt] = min;
         }
 
         return dp[amount];
+    }
+
+    public static int coinChange3(int[] coins, int amount) {
+        //dp[i] = x 表示，当目标金额为 i 时，至少需要 x 枚硬币。
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount+1);
+
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin]+1);
+                }
+            }
+        }
+        return dp[amount]==amount+1?-1:dp[amount];
     }
 
 }
