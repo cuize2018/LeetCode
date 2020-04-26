ac5ef593d2b1dd4b3c560ee743556afd43c937b5
cuize2018 <847290082@qq.com>
2020-04-26 14:44:10 +0800

    面试题19. 正则表达式匹配

---
+++ src/leet/interview/Solution19.java
@@ -0,0 +1,38 @@
+package leet.interview;
+
+public class Solution19 {
+    public static void main(String[] args) {
+        String s = "aab";
+        String p = "c*a*b";
+        System.out.println(isMatch(s, p));
+    }
+
+    public static boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+
+        for (int j = 1; j < dp[0].length; j++) {
+            dp[0][j] = p.charAt(j - 1) == '*' && dp[0][j - 2];
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
+                        dp[i][j] = dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i][j - 1] || dp[i][j - 2] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+
+    }
+}
