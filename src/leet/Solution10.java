b0419a26803f335eedba1f09144d6570cc42667d
cuize2018 <847290082@qq.com>
2020-04-26 11:22:36 +0800

    正则表达式匹配

--- src/leet/Solution10.java
+++ src/leet/Solution10.java
@@ -2,45 +2,45 @@ package leet;
 
 /**
  * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
- *
+ * <p>
  * '.' 匹配任意单个字符
  * '*' 匹配零个或多个前面的那一个元素
  * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
- *
+ * <p>
  * 说明:
- *
+ * <p>
  * s 可能为空，且只包含从 a-z 的小写字母。
  * p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
  * 示例 1:
- *
+ * <p>
  * 输入:
  * s = "aa"
  * p = "a"
  * 输出: false
  * 解释: "a" 无法匹配 "aa" 整个字符串。
  * 示例 2:
- *
+ * <p>
  * 输入:
  * s = "aa"
  * p = "a*"
  * 输出: true
  * 解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
  * 示例 3:
- *
+ * <p>
  * 输入:
  * s = "ab"
  * p = ".*"
  * 输出: true
  * 解释: ".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
  * 示例 4:
- *
+ * <p>
  * 输入:
  * s = "aab"
  * p = "c*a*b"
  * 输出: true
  * 解释: 因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
  * 示例 5:
- *
+ * <p>
  * 输入:
  * s = "mississippi"
  * p = "mis*is*p*."
@@ -48,28 +48,61 @@ package leet;
  */
 
 public class Solution10 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String s = "aab";
         String p = "c*a*b";
-        System.out.println(isMatch(s,p));
+        System.out.println(isMatch2(s, p));
     }
 
     /**
      * 回溯法
      */
     public static boolean isMatch(String s, String p) {
-        if (p.isEmpty()) return s.isEmpty();
+        if (p.length() == 0) return s.length() == 0;
 
-        boolean firstMatch = (!s.isEmpty() && (s.charAt(0)==p.charAt(0) || p.charAt(0) == '.'));
+        boolean firstMatch = s.length() != 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
 
-        if (p.length() >= 2 && p.charAt(1) == '*'){
-            return (isMatch(s, p.substring(2)) //此为第一个元素不匹配且第二个元素为'*'，此时'*'代表为0的情况
-                    || (firstMatch && isMatch(s.substring(1), p))); //此为第一个元素匹配， 此时'*'代表为多个前一元素的情况
-        }
-        else {
-            return (firstMatch && isMatch(s.substring(1), p.substring(1)));//当p的长度小于2时只用检测第一个元素即可；
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            return isMatch(s, p.substring(2))//此为第一个元素不匹配且第二个元素为'*'，此时'*'代表为0的情况
+                    || (firstMatch && isMatch(s.substring(1), p));//此为第一个元素匹配， 此时'*'代表为多个前一元素的情况
+        } else {
+            return firstMatch && isMatch(s.substring(1), p.substring(1));//当p的长度小于2时只用检测第一个元素即可；
             //当第二个元素不为'*'时, 此时需要第一个元素匹配，且剩下的p和s都匹配
         }
+    }
+
+    public static boolean isMatch2(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;//dp[i][j] 表示 s 的前 i 个是否能被 p 的前 j 个匹配
+        for (int j = 0; j < p.length(); j++) { // here's the p's length, not s's
+            if (p.charAt(j) == '*' && dp[0][j - 1]) {
+                dp[0][j + 1] = true; // here's y axis should be j+1
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {//如果是任意元素 或者是对于元素匹配
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {//如果前一个元素不匹配 且不为任意元素
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    } else {
+                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+                            /*
+                            dp[i][j] = dp[i-1][j] // 多个字符匹配的情况
+                            or dp[i][j] = dp[i][j-1] // 单个字符匹配的情况
+                            or dp[i][j] = dp[i][j-2] // 没有匹配的情况
+                             */
+
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
     }
 
 }
