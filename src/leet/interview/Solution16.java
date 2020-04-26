e446ced50194ebc4369e97968c88262e8363a4d0
cuize2018 <847290082@qq.com>
2020-04-26 10:14:41 +0800

    Pow(x,n)

--- src/leet/interview/Solution16.java
+++ src/leet/interview/Solution16.java
@@ -1,32 +1,8 @@
-package leet;
+package leet.interview;
 
-/**
- * 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
- *
- * 示例 1:
- *
- * 输入: 2.00000, 10
- * 输出: 1024.00000
- * 示例 2:
- *
- * 输入: 2.10000, 3
- * 输出: 9.26100
- * 示例 3:
- *
- * 输入: 2.00000, -2
- * 输出: 0.25000
- * 解释: 2-2 = 1/22 = 1/4 = 0.25
- * 说明:
- *
- * -100.0 < x < 100.0
- * n 是 32 位有符号整数，其数值范围是 [−231, 231 − 1] 。
- */
-public class Solution50 {
-    public static void main(String[] args){
-        double x = -1;
-        int m = -2147483647;
+public class Solution16 {
+    public static void main(String[] args) {
 
-        System.out.println(myPow(x,-2147483648));
     }
 
     /**
@@ -55,5 +31,4 @@ public class Solution50 {
         if ((n & 1) == 1) return half * half * x;
         return half * half;
     }
-
 }
