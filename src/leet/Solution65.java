909a00be89a2ceb29278e26685bcbc04fe0cfeac
Boom <37618018+cuize2018@users.noreply.github.com>
2020-05-09 22:01:03 +0800

    x 的平方根

--- src/leet/Solution65.java
+++ src/leet/Solution65.java
@@ -23,25 +23,4 @@ public class Solution65 {
         }
         return (int) left;
     }
-
-    public static int mySqrt2(int x) {
-        if (x == 0) return 0;
-        if (x == 1) return 1;
-
-        long left = 0;
-        long right = x;
-
-        while (left <= right) {
-            long middle = (left + right) >>> 1;//取右边界
-
-            if (x < middle * middle) {
-                right = middle - 1;
-            } else if (x > middle * middle) {
-                left = middle + 1;
-            } else {
-                return (int) middle;
-            }
-        }
-        return (int) Math.min(left, right);
-    }
 }
