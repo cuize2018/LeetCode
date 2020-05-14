04d4e041bb5b85b8f615c99b9a8929cb0ba766df
cuize2018 <847290082@qq.com>
2020-05-14 15:52:11 +0800

    面试题55 - I. 二叉树的深度

--- src/leet/interview/Solution55.java
+++ src/leet/interview/Solution55.java
@@ -11,20 +11,27 @@ public class Solution55 {
     }
 
     public static int maxDepth(TreeNode root) {
-        if (root == null)return 0;
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public static int maxDepth2(TreeNode root) {
+        if (root == null) return 0;
         int res = 0;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int size = queue.size();
             res++;
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.remove();
-                if (node.left != null)queue.add(node.left);
-                if (node.right != null)queue.add(node.right);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
             }
         }
         return res;
     }
+
+
 }
