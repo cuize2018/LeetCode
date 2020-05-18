a36024ba2570c4a77f514debe3859c927dbcfac6
cuize2018 <Jrl847290082>
2020-05-18 14:52:54 +0800

    面试题 17.14. 最小K个数

---
+++ src/leet/interview/Solution17_14.java
@@ -0,0 +1,59 @@
+package leet.interview;
+
+import java.util.Arrays;
+
+public class Solution17_14 {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 2, 4, 6, 8};
+//        int[] arr = {1,2,3};
+        int k = 4;
+
+        int[] res = smallestK(arr, k);
+        System.out.println(Arrays.toString(res));
+    }
+
+    public static int[] smallestK(int[] arr, int k) {
+        int len = arr.length;
+        if (len == 0) return new int[0];
+        if (len < k) return arr;
+
+        quickSearch(arr, 0, len - 1, k);
+        return Arrays.copyOfRange(arr, 0, k);
+    }
+
+    private static void quickSearch(int[] arr, int left, int right, int k) {
+        int m = partition(arr, left, right);
+        if (m == k) return;
+
+        if (m > k) {
+            quickSearch(arr, left, m - 1, k);
+        } else {
+            quickSearch(arr, m + 1, right, k);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int left = low;
+        int right = high + 1;
+        int v = arr[low];
+
+        while (true) {
+            while (arr[++left] < v) {
+                if (left == high) break;
+            }
+            while (arr[--right] > v) {
+                if (right == low) break;
+            }
+            if (left >= right) break;
+            swap(arr, left, right);
+        }
+        swap(arr, low, right);
+        return right;
+    }
+
+    private static void swap(int[] arr, int x, int y) {
+        int temp = arr[y];
+        arr[y] = arr[x];
+        arr[x] = temp;
+    }
+}
