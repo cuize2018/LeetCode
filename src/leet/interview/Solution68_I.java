3fce824ad2fa1aa5b3ce6a04e26cdce7e6628146
cuize2018 <Jrl847290082>
2020-05-18 13:49:09 +0800

    面试题68 - I. 二叉搜索树的最近公共祖先

---
+++ src/leet/interview/Solution68_I.java
@@ -0,0 +1,20 @@
+package leet.interview;
+
+import leet.TreeNode;
+
+public class Solution68_I {
+    public static void main(String[] args) {
+
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
