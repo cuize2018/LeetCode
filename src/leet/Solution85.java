4c29e2924bca358a788455fa2c2f3a84c91ea5e5
Boom <37618018+cuize2018@users.noreply.github.com>
2020-05-22 09:59:02 +0800

    最大矩形

--- src/leet/Solution85.java
+++ src/leet/Solution85.java
@@ -12,7 +12,7 @@ public class Solution85 {
                         {'1', '0', '0', '1', '0'}
                 };
 
-        System.out.println(maximalRectangle(mat));
+        System.out.println(maximalRectangle2(mat));
     }
 
     public static int maximalRectangle(char[][] matrix) {
@@ -78,4 +78,59 @@ public class Solution85 {
             }
         }
     }
+
+
+    public static int maximalRectangle2(char[][] matrix) {
+        int rows = matrix.length;
+        if (rows == 0) return 0;
+        int cols = matrix[0].length;
+
+        int max = 0;
+
+        int[] height = new int[cols + 2];
+        for (int j = 1; j < height.length - 1; j++) {
+            if (matrix[0][j - 1] == '1') height[j] = 1;
+        }
+        int area = calMaxArea(height);
+        max = Math.max(max, area);
+
+        for (int i = 1; i < rows; i++) {
+            calHeight2(matrix, i, height);//第i行的高度
+            area = calMaxArea(height);
+            max = Math.max(max, area);
+
+        }
+        return max;
+    }
+
+    private static int calMaxArea(int[] height) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        int max = 0;
+        int i = 1;
+        while (i < height.length) {
+            if (height[i] >= height[stack.peek()]) {
+                stack.push(i);
+                i++;
+            } else {
+                int j = stack.pop();
+                while (height[stack.peek()] == height[j]) {
+                    stack.pop();
+                }
+                max = Math.max((i - stack.peek() - 1) * height[j], max);
+            }
+        }
+        return max;
+    }
+
+    private static void calHeight2(char[][] matrix, int row, int[] height) {
+        for (int j = 1; j < height.length - 1; j++) {
+            if (matrix[row][j - 1] == '1') {
+                height[j] = height[j] > 0 ? height[j] + 1 : 1;
+            } else{
+                height[j] = 0;
+            }
+        }
+    }
+
 }
