434fabc51390f00045923dbca4cbc5d1c3abd856
Boom <37618018+cuize2018@users.noreply.github.com>
2020-05-24 20:26:31 +0800

    寻找两个正序数组的中位数

--- src/leet/Solution4.java
+++ src/leet/Solution4.java
@@ -69,4 +69,40 @@ public class Solution4 {
             return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
         }
     }
+
+
+    public static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+
+        int middleLeft = (len1 + len2 + 1) / 2;
+        double midVal = getKthMin(nums1, 0, len1 - 1, nums2, 0, len2 - 1, middleLeft);
+
+        if ((len1 + len2) % 2 == 0) {
+            int middleRight = (len1 + len2 + 2) / 2;
+            double midVal2 = getKthMin(nums1, 0, len1 - 1, nums2, 0, len2 - 1, middleRight);
+            return (midVal + midVal2) / 2D;
+        } else {
+            return midVal;
+        }
+    }
+
+    private static double getKthMin(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+
+        if (len2 < len1) return getKthMin(nums2, start2, end2, nums1, start1, end1, k);
+        if (len1 == 0) return nums2[start2 + k - 1];
+
+        if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+
+        int i = start1 + Math.min(len1, k / 2) - 1;
+        int j = start2 + Math.min(len2, k / 2) - 1;
+
+        if (nums1[i] < nums2[j]) {
+            return getKthMin(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+        } else {
+            return getKthMin(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+        }
+    }
 }
