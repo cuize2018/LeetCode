8f2269f9b957c3347da474b23385ef4144509f0d
cuize2018 <Jrl847290082>
2020-06-02 09:16:52 +0800

    求1+2+…+n

--- src/leet/interview/Solution64.java
+++ src/leet/interview/Solution64.java
@@ -7,7 +7,15 @@ public class Solution64 {
     }
 
     public static int sumNums(int n) {
-        if (n == 1)return 1;
-        return sumNums(n-1) + n;
+        if (n == 1) return 1;
+        return sumNums(n - 1) + n;
+    }
+
+
+    public static int sumNums2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + sumNums2(n - 1);
     }
 }
