71c91149bfc5e2ec9a32a371a621745383908bb0
cuize2018 <Jrl847290082>
2020-06-05 14:09:44 +0800

    顺时针打印矩阵

--- src/leet/interview/Solution29.java
+++ src/leet/interview/Solution29.java
@@ -5,59 +5,19 @@ import java.util.Arrays;
 public class Solution29 {
     public static void main(String[] args) {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        int[][] matrix2 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        int[][] matrix2 = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}};
+        int[][] matrix3 = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}};
 
-        int[] res = spiralOrder2(matrix2);
+        int[] res = spiralOrder3(matrix3);
         System.out.println(Arrays.toString(res));
 
-    }
-
-    public static int[] spiralOrder(int[][] matrix) {
-        int rows = matrix.length;
-        if (rows == 0) return new int[0];
-        int cols = matrix[0].length;
-
-        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int[] res = new int[rows * cols];
-        int k = 0;
-        int i = 0;
-        int j = 0;
-        int startRow;
-        int startCol;
-        boolean flag;
-        while (k < rows * cols) {
-            startRow = i;
-            startCol = j;
-            flag = false;
-            for (int[] dir : dirs) {
-                while (i >= startRow && i < rows - startRow && j >= startCol && j < cols - startCol) {
-                    res[k] = matrix[i][j];
-                    i = i + dir[0];
-                    j = j + dir[1];
-                    k++;
-                    if (i == startRow && j == startCol) {
-                        flag = true;
-                        i = startRow + 1;
-                        j = startCol + 1;
-                        break;
-                    }
-                    if (k >= rows * cols) return res;
-                }
-                if (flag) break;
-
-                if (j >= cols - startCol) {
-                    i++;
-                    j = cols - startCol - 1;
-                } else if (i >= rows - startRow) {
-                    i = rows - startRow - 1;
-                    j--;
-                } else if (j < startCol) {
-                    j = startCol;
-                    i--;
-                }
-            }
-        }
-        return res;
     }
 
     public static int[] spiralOrder2(int[][] matrix) {
@@ -99,4 +59,54 @@ public class Solution29 {
         }
         return res;
     }
+
+    public static int[] spiralOrder3(int[][] matrix) {
+        int rows = matrix.length;
+        if (rows == 0) {
+            return new int[0];
+        }
+        int cols = matrix[0].length;
+
+        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int nums = rows * cols;
+        int[] res = new int[nums];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int top = 0;
+        int down = rows - 1;
+        int left = 0;
+        int right = cols - 1;
+
+        while (k < nums) {
+            int startRow = i;
+            int startCol = j;
+            for (int m = 0; m < dirs.length; m++) {
+                int[] dir = dirs[m];
+                while (i >= top && i <= down && j >= left && j <= right && k < nums) {
+                    res[k] = matrix[i][j];
+                    i += dir[0];
+                    j += dir[1];
+                    k++;
+                    if (i == startRow && j == startCol) {
+                        break;
+                    }
+                }
+                i = Math.max(Math.min(i, down), top);
+                j = Math.max(Math.min(j, right), left);
+                if (m < dirs.length - 1) {
+                    i += dirs[m + 1][0];
+                    j += dirs[m + 1][1];
+                }
+            }
+            i++;
+            j++;
+            top++;
+            down--;
+            left++;
+            right--;
+        }
+        return res;
+    }
+
 }
