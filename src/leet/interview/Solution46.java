db6dbb9a7e9b28ee98b7678295814eeab1a53c38
Boom <37618018+cuize2018@users.noreply.github.com>
2020-06-09 10:03:03 +0800

    把数字翻译成字符串

--- src/leet/interview/Solution46.java
+++ src/leet/interview/Solution46.java
@@ -8,7 +8,7 @@ public class Solution46 {
     public static void main(String[] args) {
         int num = 506;
         Solution46 solution46 = new Solution46();
-        int ans = solution46.translateNum(num);
+        int ans = solution46.translateNum2(num);
         System.out.println(ans);
     }
 
@@ -38,6 +38,31 @@ public class Solution46 {
             one.append(dict[Integer.parseInt(temp)]);
             dfs(num.substring(i));
             one.deleteCharAt(one.length() - 1);
+        }
+    }
+
+    public int translateNum2(int num) {
+        String val = String.valueOf(num);
+        dfs2(val);
+        return res;
+    }
+
+    private void dfs2(String val) {
+        if (val.length() == 0) {
+            res++;
+            return;
+        }
+
+        for (int i = 1; i <= val.length(); i++) {
+            String temp = val.substring(0, i);
+            int v = Integer.parseInt(temp);
+            if (temp.length() > 2 || v > 25 || temp.length() > 1 && temp.charAt(0) == '0') {
+                break;
+            }
+
+            if (v >= 0) {
+                dfs2(val.substring(i));
+            }
         }
     }
 }
