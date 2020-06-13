71ade6d077c9cd96469339ea3738477a2d931aee
Boom <37618018+cuize2018@users.noreply.github.com>
2020-06-13 11:41:53 +0800

    跳台阶

--- src/leet/Solution70.java
+++ src/leet/Solution70.java
@@ -2,7 +2,7 @@ package leet;
 
 public class Solution70 {
     public static void main(String[] args) {
-        System.out.println(climbStairs(2));
+        System.out.println(climbStairs3(3));
     }
 
     public static int climbStairs(int n) {
@@ -30,4 +30,20 @@ public class Solution70 {
         }
         return dp_i;
     }
+
+
+    public static int climbStairs3(int n) {
+        if (n <= 1)return 1;
+
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+
+    }
+
 }
