5330b1ad4c516bc7faa87b114ff648d638f92177
Boom <37618018+cuize2018@users.noreply.github.com>
2020-06-25 10:00:55 +0800

    寻找两个正序数组的中位数

--- src/leet/Solution4.java
+++ src/leet/Solution4.java
@@ -70,39 +70,35 @@ public class Solution4 {
         }
     }
 
-
     public static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
         int len1 = nums1.length;
         int len2 = nums2.length;
 
-        int middleLeft = (len1 + len2 + 1) / 2;
-        double midVal = getKthMin(nums1, 0, len1 - 1, nums2, 0, len2 - 1, middleLeft);
-
+        int mid = (len1 + len2) / 2;
         if ((len1 + len2) % 2 == 0) {
-            int middleRight = (len1 + len2 + 2) / 2;
-            double midVal2 = getKthMin(nums1, 0, len1 - 1, nums2, 0, len2 - 1, middleRight);
-            return (midVal + midVal2) / 2D;
+            return (getKthMinNum(nums1, 0, len1 - 1, nums2, 0, len2 - 1, mid) +
+                    getKthMinNum(nums1, 0, len1 - 1, nums2, 0, len2 - 1, mid + 1)) / 2;
         } else {
-            return midVal;
+            return getKthMinNum(nums1, 0, len1 - 1, nums2, 0, len2 - 1, mid + 1);
         }
     }
 
-    private static double getKthMin(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
-        int len1 = end1 - start1 + 1;
-        int len2 = end2 - start2 + 1;
+    private static double getKthMinNum(int[] nums1, int l1, int r1, int[] nums2, int l2, int r2, int k) {
+        int len1 = r1 - l1 + 1;
+        int len2 = r2 - l2 + 1;
 
-        if (len2 < len1) return getKthMin(nums2, start2, end2, nums1, start1, end1, k);
-        if (len1 == 0) return nums2[start2 + k - 1];
+        if (len2 < len1) return getKthMinNum(nums2, l2, r2, nums1, l1, r1, k);
+        if (len1 == 0) return nums2[l2 + k - 1];
+        if (k == 1) return Math.min(nums1[l1], nums2[l2]);
 
-        if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+        int k1 = l1 + Math.min(len1, k / 2) - 1;
+        int k2 = l2 + Math.min(len2, k / 2) - 1;
 
-        int i = start1 + Math.min(len1, k / 2) - 1;
-        int j = start2 + Math.min(len2, k / 2) - 1;
-
-        if (nums1[i] < nums2[j]) {
-            return getKthMin(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+        if (nums1[k1] < nums2[k2]) {
+            return getKthMinNum(nums1, k1 + 1, r1, nums2, l2, r2, k - (k1 - l1 + 1));
         } else {
-            return getKthMin(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+            return getKthMinNum(nums1, l1, r1, nums2, k2 + 1, r2, k - (k2 - l2 + 1));
         }
     }
+
 }
