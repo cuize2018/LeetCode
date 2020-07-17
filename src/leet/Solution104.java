bf685a6debeadce3158deb9dbd8c6a7e6226d9bc
cuize2018 <Jrl847290082>
2020-07-17 09:22:06 +0800

    二叉树的最大深度

--- src/leet/Solution104.java
+++ src/leet/Solution104.java
@@ -1,22 +1,38 @@
 package leet;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution104 {
     public static void main(String[] args) {
 
     }
 
     public static int maxDepth(TreeNode root) {
-        int maxLen = 1;
-        maxLen = dfs(root, maxLen);
-        return  maxLen;
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
-    private static int dfs(TreeNode root, int maxLen) {
-        if (root == null)return maxLen;
+    public static int maxDepth2(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
 
-        int left = dfs(root.left, maxLen+1);
-        int right = dfs(root.right, maxLen+1);
+        int res = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
 
-        return Math.max(left, right);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            res++;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.remove();
+                if (node.left != null)queue.add(node.left);
+                if (node.right != null)queue.add(node.right);
+            }
+        }
+        return res;
     }
 }
