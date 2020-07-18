f42e0b2f7cf1ab8883396435c14d7d5a409606c9
cuize2018 <Jrl847290082>
2020-07-18 10:17:23 +0800

    交错字符串

--- src/leet/Solution97.java
+++ src/leet/Solution97.java
@@ -41,4 +41,30 @@ public class Solution97 {
         }
         return dp[s1.length()][s2.length()];
     }
+
+    public static boolean isInterleave2(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length())return false;
+
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= s2.length(); j++) {
+            dp[0][j] = s2.charAt(j - 1) == s3.charAt(j - 1);
+            if (!dp[0][j])break;
+        }
+
+        for (int i = 1; i <= s1.length(); i++) {
+            dp[i][0] = s1.charAt(i - 1) == s3.charAt(i - 1);
+            if (!dp[i][0])break;
+        }
+
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                boolean a = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+                boolean b = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                dp[i][j] = a || b;
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
+    }
 }
