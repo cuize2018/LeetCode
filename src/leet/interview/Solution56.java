6376c33fa3c90082e1bc769d70b8cce8b9fa9f99
Boom <37618018+cuize2018@users.noreply.github.com>
2020-07-19 11:13:25 +0800

    数组中数字出现的次数

--- src/leet/interview/Solution56.java
+++ src/leet/interview/Solution56.java
@@ -11,15 +11,15 @@ public class Solution56 {
 
     /**
      * 如果我们可以把所有数字分成两组，使得：
-     *      两个只出现一次的数字在不同的组中；
-     *      相同的数字会被分到相同的组中。
+     * 两个只出现一次的数字在不同的组中；
+     * 相同的数字会被分到相同的组中。
      * 那么对两个组分别进行异或操作，即可得到答案的两个数字。这是解决这个问题的关键。
-     *
+     * <p>
      * 算法:
-     *   先对所有数字进行一次异或，得到两个出现一次的数字的异或值。
-     *   在异或结果中找到任意为 1 的位。
-     *   根据这一位对所有的数字进行分组。
-     *   在每个组内进行异或操作，得到两个数字。
+     * 先对所有数字进行一次异或，得到两个出现一次的数字的异或值。
+     * 在异或结果中找到任意为 1 的位。
+     * 根据这一位对所有的数字进行分组。
+     * 在每个组内进行异或操作，得到两个数字。
      *
      * @param nums
      * @return
@@ -33,7 +33,7 @@ public class Solution56 {
         }
 
         int h = xorNum & (-xorNum);//选取的是「不为 0 的最低位」, 因为n的正数的原码的最低位1 和-n的补码的最低位1相同；
-                                    //exp ： 3(0, 011)  -3(1, 101) -> (3) & (-3) = 001
+        //exp ： 3(0, 011)  -3(1, 101) -> (3) & (-3) = 001
         int a = 0;
         int b = 0;
         for (int num : nums) {
@@ -46,5 +46,25 @@ public class Solution56 {
         ans[0] = a;
         ans[1] = b;
         return ans;
+    }
+
+    public static int[] singleNumbers2(int[] nums) {
+        if (nums.length == 2) return nums;
+        int val = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            val ^= nums[i];
+        }
+        int h = val & (-val);
+
+        int group1 = 0;
+        int group2 = 0;
+        for (int num : nums) {
+            if ((num & h) == 0) {
+                group1 ^= num;
+            } else {
+                group2 ^= num;
+            }
+        }
+        return new int[]{group1, group2};
     }
 }
