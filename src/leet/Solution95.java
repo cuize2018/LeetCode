e97656a5e593b7597ff5de771b5380408a637e9f
cuize2018 <Jrl847290082>
2020-07-21 10:41:04 +0800

    不同的二叉搜索树 II

--- src/leet/Solution95.java
+++ src/leet/Solution95.java
@@ -2,66 +2,51 @@ package leet;
 
 import com.sun.corba.se.spi.activation.ActivatorOperations;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Solution95 {
+    Map<Integer, List<TreeNode>> mem = new HashMap<>();
 
-
-    public static void main(String[] args){
-        int n = 4;
+    public static void main(String[] args) {
+        int n = 3;
         Solution95 s = new Solution95();
-        List<TreeNode> fin = s.generateTrees(4);
+        List<TreeNode> fin = s.generateTrees2(n);
+        System.out.println(fin);
         int ff = 0;
     }
 
-    /**
-     * 解法三 动态规划
-     * 通过上边的所有分析，代码可以写了，总体思想就是求长度为 2 的所有情况，
-     * 求长度为 3 的所有情况直到 n。而求长度为 len 的所有情况，我们只需要求出一个代表 [ 1 2 ... len ] 的所有情况，
-     * 其他的话加上一个偏差，加上当前根节点即可。
-     * https://leetcode-cn.com/problems/unique-binary-search-trees-ii/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-2-7/
-     * @param n
-     * @return
-     */
-    public static List<TreeNode> generateTrees(int n) {
-        ArrayList<TreeNode>[] dp = new ArrayList[n + 1];
-        dp[0] = new ArrayList<>();
+    public List<TreeNode> generateTrees2(int n) {
+        if (n <= 0) return new ArrayList<>();
+        return getTrees(1, n);
+    }
 
-        if (n == 0){
-            return dp[0];
+    private List<TreeNode> getTrees(int left, int right) {
+        int key = left * 8 + right;
+        if (mem.containsKey(key)) {
+            return mem.get(key);
         }
-        dp[0].add(null);
 
-        for (int len = 1;len <= n;len++){ //长度从1~n
-            dp[len] = new ArrayList<>();
-            for (int root = 1;root <= len;root++){//根节点从1~len
-                int left = root-1;
-                int right = len-root;
+        List<TreeNode> res = new ArrayList<>();
+        if (left > right) {
+            res.add(null);
+            return res;
+        }
 
-                for (TreeNode LeftNode: dp[left]){
-                    for (TreeNode RightNode: dp[right]){
-                        TreeNode rootNode = new TreeNode(root);
-                        rootNode.left = LeftNode;
-                        rootNode.right = copy(RightNode, root);
-                        dp[len].add(rootNode);
-                    }
+        for (int rootVal = left; rootVal <= right; rootVal++) {
+            List<TreeNode> lefts = getTrees(left, rootVal - 1);
+            List<TreeNode> rights = getTrees(rootVal + 1, right);
+
+            for (TreeNode l : lefts) {
+                for (TreeNode r : rights) {
+                    TreeNode root = new TreeNode(rootVal);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
                 }
             }
         }
-        return dp[n];
-    }
-
-    private static  TreeNode copy(TreeNode root, int offset){
-        if (root == null){
-            return null;
-        }
-        TreeNode outt = new TreeNode(root.val+offset);
-        outt.left = copy(root.left, offset);
-        outt.right = copy(root.right,offset);
-        return outt;
+        mem.put(key, res);
+        return res;
     }
 
 }
