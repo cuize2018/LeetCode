cd7b930084dcbcbee13420ba8cbf8c54acde83b2
cuize2018 <Jrl847290082>
2020-07-21 11:14:22 +0800

    剑指 Offer 26. 树的子结构

--- src/leet/interview/Solution26.java
+++ src/leet/interview/Solution26.java
@@ -4,37 +4,6 @@ import leet.TreeNode;
 
 public class Solution26 {
     public static void main(String[] args) {
-//        TreeNode root = new TreeNode(4);root.left = new TreeNode(2);root.right = new TreeNode(3);
-//        TreeNode mov = root;
-//        mov = mov.left;
-//        mov.left = new TreeNode(4);
-//        mov.right = new TreeNode(5);
-//
-//        mov = root.right;
-//        mov.left = new TreeNode(6);
-//        mov.right = new TreeNode(7);
-//
-//        mov = root.left.left;
-//        mov.left = new TreeNode(8);
-//        mov.right = new TreeNode(9);
-//
-//        TreeNode B = new TreeNode(4);
-//        B.left = new TreeNode(8);
-//        B.right = new TreeNode(9);
-
-
-//        TreeNode root = new TreeNode(1);root.left = new TreeNode(0);root.right = new TreeNode(1);
-//        TreeNode mov = root;
-//        mov = mov.left;
-//        mov.left = new TreeNode(-4);
-//        mov.right = new TreeNode(3);
-//
-//
-//        TreeNode B = new TreeNode(1);
-//        B.left = new TreeNode(-4);
-
-
-
         TreeNode root = new TreeNode(10);root.left = new TreeNode(12);root.right = new TreeNode(6);
         TreeNode mov = root;
         mov = mov.left;
@@ -71,6 +40,18 @@ public class Solution26 {
         if (B == null)return true;
         if (A == null)return false;
         return A.val == B.val && dfs(A.left,B.left) && dfs(A.right,B.right);
+    }
+
+
+    public boolean isSubStructure2(TreeNode A, TreeNode B) {
+        if (A == null || B == null)return false;
+        return dfs2(A,B) || isSubStructure2(A.left, B) || isSubStructure2(A.right, B);
+    }
+
+    private boolean dfs2(TreeNode A, TreeNode B) {
+        if (B == null)return true;
+        if (A == null)return false;
+        return A.val == B.val && dfs2(A.left, B.left) && dfs2(A.right, B.right);
     }
 
 
