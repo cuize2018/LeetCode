8fd4dbee27b1cdec8c9bb69f69ac6c66c2d696eb
cuize2018 <Jrl847290082>
2020-07-23 10:13:36 +0800

    最小路径和

--- src/leet/Solution64.java
+++ src/leet/Solution64.java
@@ -26,19 +26,25 @@ public class Solution64 {
     }
 
     public static int minPathSum(int[][] grid) {
-        int[][] len = grid.clone();
-        for (int j = 1;j<grid[0].length;j++){
-            len[0][j] = len[0][j] + len[0][j-1];
+        int rows = grid.length;
+        if (rows == 0)return 0;
+        int cols = grid[0].length;
+
+        int[][] dp = new int[rows][cols];
+        dp[0][0] = grid[0][0];
+
+        for (int j = 1; j < cols; j++) {
+            dp[0][j] = dp[0][j-1]+grid[0][j];
         }
-        for (int j = 1;j<grid.length;j++){
-            len[j][0] = len[j][0] + len[j-1][0];
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i-1][0]+grid[i][0];
         }
 
-       for (int i = 1;i<grid.length;i++){
-           for (int j = 1;j<grid[0].length;j++){
-               len[i][j] = Math.min(len[i][j] + len[i-1][j], len[i][j]+len[i][j-1]);
-           }
-       }
-       return len[grid.length-1][grid[0].length-1];
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+            }
+        }
+        return dp[rows-1][cols-1];
     }
 }
