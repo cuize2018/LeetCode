38e3bea0ea56350c3cd13fc9133b243d8786584f
cuize2018 <Jrl847290082>
2020-07-24 21:06:31 +0800

    除数博弈

---
+++ src/leet/Solution1025.java
@@ -0,0 +1,26 @@
+package leet;
+
+public class Solution1025 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean divisorGame(int N) {
+        if (N == 1)return false;
+        if (N == 2)return true;
+
+        boolean[] dp = new boolean[N+1];
+        dp[1] = false;
+        dp[2] = true;
+
+        for (int i = 3; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0 &&  !dp[i - j]){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[N];
+    }
+}
