27a5dae011e81347073c6d69dc99d00bcc1bb9f8
cuize2018 <Jrl847290082>
2020-07-29 10:55:39 +0800

    实现 Trie (前缀树)

--- src/leet/Solution208.java
+++ src/leet/Solution208.java
@@ -4,65 +4,67 @@ import java.util.HashSet;
 
 public class Solution208 {
     public static void main(String[] args) {
-
+        Trie trie2 = new Trie();
+        trie2.insert("world");
     }
 }
 
 class Trie {
-    private TrieNode root;
-
-    /**
-     * Initialize your data structure here.
-     */
+    TrieNode2 root;
+    /** Initialize your data structure here. */
     public Trie() {
-        root = new TrieNode();
+        root = new TrieNode2('#');
     }
 
-    /**
-     * Inserts a word into the trie.
-     */
+    /** Inserts a word into the trie. */
     public void insert(String word) {
-        TrieNode curr = root;
-
+        TrieNode2 mov = root;
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if (curr.getChildren()[c - 'a'] == null) {
-                curr.getChildren()[c - 'a'] = new TrieNode(c);
+            if (mov.children[c - 'a'] == null){
+                mov.children[c - 'a'] = new TrieNode2(c);
             }
-            curr = curr.getChildren()[c - 'a'];
+            mov = mov.children[c-'a'];
         }
-        curr.setEnd();
+        mov.isEnd = true;
     }
 
-    /**
-     * Returns if the word is in the trie.
-     */
+    /** Returns if the word is in the trie. */
     public boolean search(String word) {
-        TrieNode curr = root;
-
+        if (word == null)return false;
+        TrieNode2 mov = root;
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if (curr.getChildren()[c - 'a'] == null) {
+            if (mov.children[c-'a'] == null){
                 return false;
             }
-            curr = curr.getChildren()[c - 'a'];
+            mov = mov.children[c-'a'];
         }
-        return curr.isEnd();
+        return mov.isEnd;
     }
 
-    /**
-     * Returns if there is any word in the trie that starts with the given prefix.
-     */
+    /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-        TrieNode curr = root;
-
+        TrieNode2 mov = root;
         for (int i = 0; i < prefix.length(); i++) {
             char c = prefix.charAt(i);
-            if (curr.getChildren()[c - 'a'] == null) {
+            if (mov.children[c-'a'] == null){
                 return false;
             }
-            curr = curr.getChildren()[c - 'a'];
+            mov = mov.children[c-'a'];
         }
         return true;
+    }
+}
+
+class TrieNode2{
+    char val;
+    TrieNode2[] children;
+    boolean isEnd;
+
+    public TrieNode2( char v){
+        val = v;
+        children = new TrieNode2[26];
+        isEnd = false;
     }
 }
