b342705d49c48a67505b985fe095426930677ca5
cuize2018 <Jrl847290082>
2020-07-29 11:31:06 +0800

    丑数

--- src/leet/Solution263.java
+++ src/leet/Solution263.java
@@ -26,4 +26,23 @@ public class Solution263 {
         }
         return true;
     }
+
+    public static boolean isUgly2(int num) {
+        if (num == 0)return true;
+        if (num == 1)return false;
+
+        while (num != 1){
+            if (num % 2 == 0){
+                num = num / 2;
+            }
+            else if (num % 3 == 0){
+                num /= 3;
+            }
+            else if (num % 5 == 0){
+                num /= 5;
+            }
+            else return false;
+        }
+        return true;
+    }
 }
