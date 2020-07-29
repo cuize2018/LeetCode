41889e03c6b31d48a71a867c1999b16871bc2ad9
cuize2018 <Jrl847290082>
2020-07-29 10:55:58 +0800

    缺失的第一个正数

--- src/leet/Solution41.java
+++ src/leet/Solution41.java
@@ -37,7 +37,7 @@ public class Solution41 {
      * 以题目中的示例二 [3, 4, -1, 1] 为例，恢复后的数组应当为 [1, -1, 3, 4]，我们就可以知道缺失的数为 2
      *
      * 那么我们如何将数组进行恢复呢？我们可以对数组进行一次遍历，对于遍历到的数 x = nums[i]，
-     * 如果x∈[1,N]，我们就知道 x 应当出现在数组中的 x - 1 的位置，因此交换 nums[i] 和 nums[x−1]，这样 xx 就出现在了正确的位置。
+     * 如果x∈[1,N]，我们就知道 x 应当出现在数组中的 x - 1 的位置，因此交换 nums[i] 和 nums[x−1]，这样 x 就出现在了正确的位置。
      * 在完成交换后，新的 nums[i] 可能还在 [1, N] 的范围内，我们需要继续进行交换操作，直到 x∈[1,N]。
      *
      * 注意到上面的方法可能会陷入死循环。如果 nums[i] 恰好与 nums[x−1] 相等，那么就会无限交换下去。
@@ -78,5 +78,27 @@ public class Solution41 {
             res++;
         }
         return res;
+    }
+
+    public static int firstMissingPositive3(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] <= n && nums[i] > 0 && nums[nums[i] - 1] != nums[i]){
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i]-1 != i){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        nums[i] = nums[i] ^ nums[j];
+        nums[j] = nums[i] ^ nums[j];
+        nums[i] = nums[i] ^ nums[j];
     }
 }
