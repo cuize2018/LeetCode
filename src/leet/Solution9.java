7b9f27bc8d56450b215027eb31f45a7eefc37df0
Boom <37618018+cuize2018@users.noreply.github.com>
2020-07-30 23:02:47 +0800

    回文数

--- src/leet/Solution9.java
+++ src/leet/Solution9.java
@@ -1,6 +1,8 @@
 package leet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -27,6 +29,18 @@ public class Solution9 {
         System.out.println(isPalindrome(num));
     }
 
+    /**
+     * 通过取整和取余操作获取整数中对应的数字进行比较。
+     *
+     * 举个例子: 1221 这个数字。
+     *
+     * 通过计算 1221 / 1000， 得首位1
+     * 通过计算 1221 % 10， 可得末位 1
+     * 进行比较
+     * 再将 22 取出来继续比较
+     * @param x
+     * @return
+     */
     public static boolean isPalindrome(int x){
         if (x < 0)return false;
         if (x < 10)return true;
