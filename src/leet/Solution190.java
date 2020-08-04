d74d62b518b421f0577a7ad9f9b14cf554bbbb33
cuize2018 <Jrl847290082>
2020-08-04 14:29:35 +0800

    颠倒二进制位

--- src/leet/Solution190.java
+++ src/leet/Solution190.java
@@ -24,4 +24,33 @@ public class Solution190 {
         }
         return v;
     }
+
+    public static int reverseBits2(int n) {
+        int v = 1;
+        long k = Integer.MAX_VALUE + 1L;
+
+        for (int i = 0; i < 16; i++) {
+            n = swap(n, v, k);
+            v = v << 1;
+            k = k >>> 1;
+        }
+        return n;
+    }
+
+    private static int swap(long n, int i, long j) {
+        long low = n & i;
+        long high = n & j;
+        if (low == 0 && high == 0)return (int)n;
+        if (low != 0 && high != 0)return (int)n;
+
+        if (high == 0){
+            n = n ^ i;
+            n = n | j;
+        }
+        if (low == 0){
+            n = n ^ j;
+            n = n | i;
+        }
+        return (int)n;
+    }
 }
