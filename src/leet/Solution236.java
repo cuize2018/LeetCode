f50875f2d1e58106cf482a1fd34cb5473590b21a
cuize2018 <Jrl847290082>
2020-08-05 10:30:11 +0800

    二叉树的最近公共父节点

--- src/leet/Solution236.java
+++ src/leet/Solution236.java
@@ -62,4 +62,17 @@ public class Solution236 {
     }
 
 
+    public TreeNode lowestCommonAncestor3(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)return null;
+        if (root.val == p.val || root.val == q.val)return root;
+
+        TreeNode left = lowestCommonAncestor3(root.left, p, q);
+        TreeNode right = lowestCommonAncestor3(root.right, p, q);
+        if (left != null && right != null)return root;
+
+        if (left == null)return right;
+        return left;
+    }
+
+
 }
