515fc3bc97e9d56598a36aa77a103e46d6df3060
Boom <37618018+cuize2018@users.noreply.github.com>
2020-08-09 11:11:40 +0800

    复原IP地址

--- src/leet/Solution93.java
+++ src/leet/Solution93.java
@@ -1,62 +1,44 @@
 package leet;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Solution93 {
-    private Set<String> res = new HashSet<>();
-    public static void main(String[] args){
+    private List<String> res = new ArrayList<>();
+
+    public static void main(String[] args) {
         String s = "25525511135";
         String s1 = "010010";
         System.out.println(new Solution93().restoreIpAddresses(s1));
     }
 
-    public  List<String> restoreIpAddresses(String s){
-        if (s.length() >= 4) {
-            backTrack(1, s, "", 0);
-            backTrack(2, s, "", 0);
-            backTrack(3, s, "", 0);
-        }
-        else {
-            return new ArrayList<>();
-        }
-        return new ArrayList<>(res);
+    public List<String> restoreIpAddresses(String s) {
+        if (s.length() < 4) return new ArrayList<>();
+
+        List<String> str = new ArrayList<>();
+        dfs(str, s);
+        return res;
     }
 
-    private void backTrack(int num, String rest, String now, int level){
-        if (level == 4 && !rest.isEmpty()){
+    private void dfs(List<String> str, String s) {
+        if (str.size() == 4 && s.length() > 0) return;
+
+        if (s.length() == 0 && str.size() == 4) {
+            res.add(String.join(".", str));
             return;
         }
-        if (rest.isEmpty() && level == 4){
-            res.add(now);
-            return;
-        }
 
-        if (rest.length() >= num) {
-            String temp;
-            String next = rest.substring(0,num);
-            int val = Integer.parseInt(next);
-            if (val < 0 || val > 255){
-                return;
+        for (int i = 1; i <= Math.min(s.length(), 3); i++) {
+            String curr = s.substring(0, i);
+            int v = Integer.parseInt(curr);
+
+            if (v != 0 && curr.charAt(0) == '0') return;
+            if (v == 0 && curr.length() > 1) return;
+
+            if (v <= 255 && v >= 0) {
+                str.add(curr);
+                dfs(str, s.substring(i));
+                str.remove(str.size() - 1);
             }
-
-            if (val != 0 && next.charAt(0) == '0'){
-                return;
-            }
-            if (val == 0 && next.length() > 1){
-                return;
-            }
-
-            if (!now.isEmpty()) temp = now + "." + next;
-            else temp = next;
-
-            String rest_new = rest.substring(num);
-
-            backTrack(1, rest_new, temp, level+1);
-            backTrack(2, rest_new, temp, level+1);
-            backTrack(3, rest_new, temp, level+1);
         }
     }
 }
