aeeb4f69bc7f3fc7a40fa4782e959a4cdae92caa
Boom <37618018+cuize2018@users.noreply.github.com>
2020-08-12 10:14:06 +0800

    有序数组中的单一元素

--- src/leet/Solution540.java
+++ src/leet/Solution540.java
@@ -11,11 +11,10 @@ public class Solution540 {
      * 当中间数的下标为奇数，说明前后元素的个数为奇数，偶数则剩余个数为偶数。
      * 奇数时：当nums[h]等于[h+1],唯一数处于前h-1,反之处于后h+1。
      * 偶数时：当nums[h]等于[h+1]，唯一数处于后h+2,反之处于前h.
-     *
+     * <p>
      * 目的是要保持剩余查找元素个数为奇数，因为要查找的是唯一仅出现一次的元素，含有唯一数的数组长度必为奇数
      * (就是要保证剩余查找元素个数奇数)
      *
-
      * @param nums
      * @return
      */
@@ -38,7 +37,7 @@ public class Solution540 {
                 //若nums[middle]==nums[middle+1]， 则[middle,end]个数为奇数，则[middle+2,end]个数为奇数，所以[middle,end]存在唯一数
                 if (nums[middle] == nums[middle + 1])
                     low = middle;
-                //若nums[middle]==nums[middle-1]， 则[low,middle]个数为奇数，则[low,middle-2]个数为奇数，所以[low,middle]存在唯一数
+                    //若nums[middle]==nums[middle-1]， 则[low,middle]个数为奇数，则[low,middle-2]个数为奇数，所以[low,middle]存在唯一数
                 else
                     high = middle;
             }
@@ -48,19 +47,47 @@ public class Solution540 {
 
     /**
      * 滑动窗口
+     *
      * @param nums
      * @return
      */
     public static int singleNonDuplicate2(int[] nums) {
         int len = nums.length;
-        if (len == 1)return nums[0];
+        if (len == 1) return nums[0];
 
         int i = 0;
-        for (;i <= nums.length-2;i+=2){
-            if (nums[i] != nums[i+1]){
+        for (; i <= nums.length - 2; i += 2) {
+            if (nums[i] != nums[i + 1]) {
                 return nums[i];
             }
         }
         return nums[i];
+    }
+
+
+    public static int singleNonDuplicate3(int[] nums) {
+        if (nums.length == 0) return -1;
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = (low + high) >>> 1;
+
+            if ((mid % 2) == 1) {
+                if (nums[mid] == nums[mid + 1]) {
+                    high = mid - 1;
+                } else if (nums[mid] == nums[mid - 1]) {
+                    low = mid + 1;
+                } else return nums[mid];
+            } else {
+                if (nums[mid] == nums[mid + 1]) {
+                    low = mid;
+                } else if (nums[mid] == nums[mid - 1]) {
+                    high = mid;
+                } else return nums[mid];
+            }
+        }
+        return nums[low];
     }
 }
