a7208f8802a50fe93883aac96ae3f9f7cd5fa509
Boom <37618018+cuize2018@users.noreply.github.com>
2020-08-14 09:30:20 +0800

    有效的括号

--- src/leet/Solution20.java
+++ src/leet/Solution20.java
@@ -5,111 +5,81 @@ import java.util.Stack;
 
 /**
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
- *
+ * <p>
  * 有效字符串需满足：
- *
+ * <p>
  * 左括号必须用相同类型的右括号闭合。
  * 左括号必须以正确的顺序闭合。
  * 注意空字符串可被认为是有效字符串。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: "()"
  * 输出: true
  * 示例 2:
- *
+ * <p>
  * 输入: "()[]{}"
  * 输出: true
  * 示例 3:
- *
+ * <p>
  * 输入: "(]"
  * 输出: false
  * 示例 4:
- *
+ * <p>
  * 输入: "([)]"
  * 输出: false
  * 示例 5:
- *
+ * <p>
  * 输入: "{[]}"
  * 输出: true
  */
 public class Solution20 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String s = "([]){}";
-        System.out.println(isValid2(s));
+        System.out.println(isValid(s));
     }
 
     public static boolean isValid(String s) {
-        if (s.isEmpty())return true;
-        HashMap<Character, Integer> level = new HashMap<>();
+        if (s.length() == 0) return true;
+        if (s.length() % 2 != 0) return false;
+
         Stack<Character> stack = new Stack<>();
-        int currLevel = 10;
-        int count = 0;
-
-        level.put('(', 1);
-        level.put(')', 1);
-
-        level.put('[', 2);
-        level.put(']', 2);
-
-        level.put('{', 3);
-        level.put('}', 3);
-
-        for (char c : s.toCharArray()){
-            if (level.get(c) != currLevel || (c == stack.peek() && (count+1)<s.length())){
+        int i = 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
-                currLevel = level.get(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char t = stack.pop();
+                if (t == '(' && c != ')') return false;
+                if (t == '[' && c != ']') return false;
+                if (t == '{' && c != '}') return false;
             }
-            else {
-                char x = stack.pop();
-                if (stack.size() > 0){
-                    currLevel = level.get(stack.peek());
-                }
-                else {
-                    currLevel = 10;
-                }
-                if (x == c || level.get(x) != level.get(c)){
-                    return false;
-                }
-            }
-            count++;
+            i++;
         }
-        if (stack.isEmpty()) return true;
-        else return false;
+        return stack.isEmpty();
     }
 
     public static boolean isValid2(String s) {
-        if (s.isEmpty())return true;
-        HashMap<Character, Integer> level = new HashMap<>();
-        HashMap<Character, Integer> level2 = new HashMap<>();
+        int n = s.length();
+        if (n == 0) return true;
+        char[] chars = s.toCharArray();
         Stack<Character> stack = new Stack<>();
-
-        level.put('(', 1);
-        level.put('[', 2);
-        level.put('{', 3);
-
-        level2.put(')',1);
-        level2.put(']',2);
-        level2.put('}',3);
-
-        for (char c : s.toCharArray()){
-            if (level.containsKey(c)){
+        for (char c : chars) {
+            if (stack.isEmpty()) {
+                if (c == ')' || c == ']' || c == '}') return false;
                 stack.push(c);
-            }
-            else {
-                char x;
-                if (!stack.isEmpty()){
-                    x = stack.pop();
-                }
-                else {
-                    return false;
-                }
-                if (level.get(x) != level2.get(c)){
-                    return false;
-                }
+            } else {
+                if (c == ')' && stack.peek() == '(') stack.pop();
+                else if (c == ']' && stack.peek() == '[') stack.pop();
+                else if (c == '}' && stack.peek() == '{') stack.pop();
+                else if (c == '(' || c == '[' || c == '{') {
+                    stack.push(c);
+                } else return false;
             }
         }
-        if (stack.isEmpty()) return true;
-        else return false;
+        return stack.isEmpty();
     }
+
 }
