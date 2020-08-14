5a69568bb392d2251577f72ac0b68cdd57f0c5b8
Boom <37618018+cuize2018@users.noreply.github.com>
2020-08-14 10:23:48 +0800

    面试题 08.11. 硬币

--- src/leet/interview/Solution08_11.java
+++ src/leet/interview/Solution08_11.java
@@ -1,9 +1,11 @@
 package leet.interview;
 
+import java.util.Arrays;
+
 public class Solution08_11 {
     public static void main(String[] args) {
-        int n = 10;
-        System.out.println(waysToChange(n));
+        int n = 5;
+        System.out.println(waysToChange3(n));
     }
 
     public static int waysToChange(int n) {
@@ -13,7 +15,7 @@ public class Solution08_11 {
 
         int[] coins = {1, 5, 10, 25};
 
-        for (int j = 1;j<4;j++) {
+        for (int j = 0; j < 4; j++) {
             for (int i = coins[j]; i <= n; i++) {
                 dp[i] += dp[i - j] % 1000000007;
             }
@@ -25,15 +27,14 @@ public class Solution08_11 {
      * dp[i][j]=dp[i-1][j]+dp[i][j-coins[i]]
      * 令 dp[i][j] 为遍历到当下这个硬币时，组成金额 j 的方法数目
      * 有两种可能性（1）当前这个硬币没有取，dp[i][j] = dp[i-1][j]；
-     *           （2）当前这个硬币取了，dp[i][j] = dp[i][j-coins[i]]。最后的结果是两者的和
+     * （2）当前这个硬币取了，dp[i][j] = dp[i][j-coins[i]]。最后的结果是两者的和
      *
-
      * @param n
      * @return
      */
     public static int waysToChange2(int n) {
         int[] coins = {1, 5, 10, 25};
-        int[][] dp = new int[4][n+1];
+        int[][] dp = new int[4][n + 1];
 
         for (int j = 0; j <= n; j++) {
             dp[0][j] = 1;
@@ -45,12 +46,27 @@ public class Solution08_11 {
 
         for (int i = 1; i < 4; i++) {
             for (int j = 1; j <= n; j++) {
-                dp[i][j] = dp[i-1][j];
-                if (j - coins[i] >= 0){
-                    dp[i][j] += dp[i][j-coins[i]]%1000000007;
+                dp[i][j] = dp[i - 1][j];
+                if (j - coins[i] >= 0) {
+                    dp[i][j] += dp[i][j - coins[i]] % 1000000007;
                 }
             }
         }
         return dp[3][n];
+    }
+
+    public static int waysToChange3(int n) {
+        if (n == 0) return 0;
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        int[] coins = {25, 10, 5, 1};
+
+        for (int coin : coins) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] += dp[i - coin] % 1000000007;
+            }
+        }
+        return dp[n] % 1000000007;
     }
 }
