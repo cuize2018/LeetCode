63102bbb5ed46613af4c9b6afc1ec2588a9a2470
Boom <37618018+cuize2018@users.noreply.github.com>
2020-08-17 23:19:19 +0800

    二叉树的锯齿形层次遍历

--- src/leet/Solution103.java
+++ src/leet/Solution103.java
@@ -1,10 +1,11 @@
 package leet;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution103 {
-    private List<List<Integer>> out = new ArrayList<>();
     public static void main(String[] args){
         TreeNode root = new TreeNode(3);
         TreeNode rootCopy = root;
@@ -14,66 +15,36 @@ public class Solution103 {
         root.left = new TreeNode(15);
         root.right = new TreeNode(7);
 
-        Solution103 s = new Solution103();
-        List<List<Integer>> fin = s.zigzagLevelOrder(rootCopy);
+        List<List<Integer>> fin = zigzagLevelOrder(rootCopy);
         System.out.println(fin.toString());
     }
 
-    public  List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> out = new ArrayList<>();
         if (root == null)return out;
-        if (root.left == null && root.right == null){
-            List<Integer> l = new ArrayList<>();
-            l.add(root.val);
-            out.add(l);
-            return out;
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.add(root);
+        int level = 1;
+        while (!deque.isEmpty()){
+            int size = deque.size();
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                if (level % 2 == 1) {
+                    TreeNode node = deque.removeFirst();
+                    temp.add(node.val);
+                    if (node.left != null)deque.addLast(node.left);
+                    if (node.right != null)deque.addLast(node.right);
+                }
+                else {
+                    TreeNode node = deque.removeLast();
+                    temp.add(node.val);
+                    if (node.right != null) deque.addFirst(node.right);
+                    if (node.left != null) deque.addFirst(node.left);
+                }
+            }
+            out.add(temp);
+            level++;
         }
-
-        List<TreeNode> lastLevel = new ArrayList<>();
-        List<Integer> val = new ArrayList<>();
-        lastLevel.add(root);
-        val.add(root.val);
-        out.add(val);
-
-        getLevel(lastLevel,1);
         return out;
-    }
-
-    private  void getLevel(List<TreeNode> lastLevel, int level){
-        List<TreeNode> thisLevel = new ArrayList<>();
-        List<Integer> val = new ArrayList<>();
-
-        for (int i = lastLevel.size() - 1; i >= 0; i--) {
-            TreeNode node = lastLevel.get(i);
-
-            TreeNode leftNode = node.left;
-            TreeNode rightNode = node.right;
-
-            if (level % 2 == 0) {
-                if (leftNode != null) {
-                    thisLevel.add(leftNode);
-                    val.add(leftNode.val);
-                }
-                if (rightNode != null) {
-                    thisLevel.add(rightNode);
-                    val.add(rightNode.val);
-                }
-            }
-            else {
-                if (rightNode != null) {
-                    thisLevel.add(rightNode);
-                    val.add(rightNode.val);
-                }
-                if (leftNode != null) {
-                    thisLevel.add(leftNode);
-                    val.add(leftNode.val);
-                }
-            }
-        }
-
-        if (thisLevel.isEmpty()){
-            return;
-        }
-        out.add(val);
-        getLevel(thisLevel, level+1);
     }
 }
