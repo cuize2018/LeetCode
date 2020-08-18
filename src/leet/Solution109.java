af5e554cf8a52a95aa1935a239da6e81b6e987b0
Boom <37618018+cuize2018@users.noreply.github.com>
2020-08-18 10:57:01 +0800

    有序链表转换二叉搜索树

--- src/leet/Solution109.java
+++ src/leet/Solution109.java
@@ -1,58 +1,41 @@
 package leet;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution109 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ListNode l = new ListNode(-10);
         ListNode h = l;
-        l.next = new ListNode(-3);l=l.next;
-        l.next = new ListNode(0);l=l.next;
-        l.next = new ListNode(5);l=l.next;
-        l.next = new ListNode(9);l=l.next;
+        l.next = new ListNode(-3);
+        l = l.next;
+        l.next = new ListNode(0);
+        l = l.next;
+        l.next = new ListNode(5);
+        l = l.next;
+        l.next = new ListNode(9);
+        l = l.next;
 
         TreeNode out = sortedListToBST(h);
-        int ff = 0;
     }
 
     public static TreeNode sortedListToBST(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        ListNode mov = head;
-        while (mov != null){
-            list.add(mov.val);
-            mov = mov.next;
+        if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+
+        ListNode slow = head;
+        ListNode slowPre = null;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slowPre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
+        TreeNode root = new TreeNode(slow.val);
+        slowPre.next = null;
+        TreeNode left = sortedListToBST(head);
+        TreeNode right = sortedListToBST(slow.next);
 
-        if (list.isEmpty())return null;
-        if (list.size() == 1)return new TreeNode(list.get(0));
-
-        int mid = list.size()/2;
-        TreeNode root = new TreeNode(list.get(mid));
-
-        List<Integer> left = list.subList(0,mid);
-        List<Integer> right = list.subList(mid+1, list.size());
-
-        root.left = buildSub(left);
-        root.right = buildSub(right);
-
-        return root;
-    }
-
-
-    private static TreeNode buildSub(List<Integer> list){
-        if (list.isEmpty())return null;
-        if (list.size() == 1)return new TreeNode(list.get(0));
-
-        int mid = list.size()/2;
-        TreeNode root = new TreeNode(list.get(mid));
-
-        List<Integer> left = list.subList(0,mid);
-        List<Integer> right = list.subList(mid+1, list.size());
-
-        root.left = buildSub(left);
-        root.right = buildSub(right);
-
+        root.left = left;
+        root.right = right;
         return root;
     }
 
