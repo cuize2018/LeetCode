1bd13d3bcc03d63ac5bf80fb353f64714ffcad27
Boom <37618018+cuize2018@users.noreply.github.com>
2020-08-19 10:34:34 +0800

    螺旋矩阵

--- src/leet/Solution54.java
+++ src/leet/Solution54.java
@@ -5,109 +5,72 @@ import java.util.List;
 
 /**
  * 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入:
  * [
- *  [ 1, 2, 3 ],
- *  [ 4, 5, 6 ],
- *  [ 7, 8, 9 ]
+ * [ 1, 2, 3 ],
+ * [ 4, 5, 6 ],
+ * [ 7, 8, 9 ]
  * ]
  * 输出: [1,2,3,6,9,8,7,4,5]
  * 示例 2:
- *
+ * <p>
  * 输入:
  * [
- *   [1, 2, 3, 4],
- *   [5, 6, 7, 8],
- *   [9,10,11,12]
+ * [1, 2, 3, 4],
+ * [5, 6, 7, 8],
+ * [9,10,11,12]
  * ]
  * 输出: [1,2,3,4,8,12,11,10,9,5,6,7]
  */
 public class Solution54 {
-    public static void main(String[] args){
-        int[][] mat = {};
-        List<Integer> out = spiralOrder(mat);
+    public static void main(String[] args) {
+        int[][] mat = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        int[][] mat2 = {{1, 2, 3}};
+        List<Integer> out = spiralOrder2(mat);
 
         System.out.println(out);
     }
 
-    public static List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> out = new ArrayList<>();
-        if (matrix.length == 0){return out;}
+    public static List<Integer> spiralOrder2(int[][] matrix) {
+        int rows = matrix.length;
+        if (rows == 0) return new ArrayList<>();
+        int cols = matrix[0].length;
 
-        int numRows = matrix.length;
-        int numCols = matrix[0].length;
-
-        int curRow = 0;
-        int curCol = 0;
-
-        boolean goDown = false;
-        boolean goUp = false;
-
-        boolean goLeft = false;
-        boolean goRight = true;
-        int count = 0;
-
-        int upRow = numRows;
-        int lowRow = 0;
-        int upCol = numCols;
-        int lowCol = 0;
-
-        while (count < numCols*numRows) {
-            while (curRow < upRow && curRow >=lowRow && curCol < upCol && curCol >= lowCol) {
-                if (goRight) {
-                    out.add(matrix[curRow][curCol]);
-                    curCol++;
-                    count++;
-                } else if (goDown) {
-                    out.add(matrix[curRow][curCol]);
-                    curRow++;
-                    count++;
-                } else if (goUp) {
-                    out.add(matrix[curRow][curCol]);
-                    curRow--;
-                    count++;
-                } else if (goLeft) {
-                    out.add(matrix[curRow][curCol]);
-                    curCol--;
-                    count++;
+        List<Integer> res = new ArrayList<>();
+        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int up = 0;
+        int down = rows - 1;
+        int left = 0;
+        int right = cols - 1;
+        int i = up, j = left;
+        while (up <= down && left <= right) {
+            res.add(matrix[i][j++]);
+            for (int k = 0; k < dirs.length; k++) {
+                int[] dir = dirs[k];
+                while (i >= up && i <= down && j >= left && j <= right) {
+                    if (i == up && j == left) break;
+                    res.add(matrix[i][j]);
+                    i += dir[0];
+                    j += dir[1];
                 }
+                i = Math.max(up, Math.min(i, down));
+                j = Math.max(left, Math.min(j, right));
+                if (k < dirs.length - 1) {
+                    i += dirs[k + 1][0];
+                    j += dirs[k + 1][1];
+                }
+                if (i < up || i > down || j < left || j > right) break;
             }
-
-            if (goRight){
-                goDown = !goDown;
-                goRight = !goRight;
-                curCol--;
-                curRow++;
-            }
-            else if (goDown){
-                goDown = !goDown;
-                goLeft = !goLeft;
-                curRow--;
-                curCol--;
-            }
-            else if (goLeft){
-                goLeft = !goLeft;
-                goUp = !goUp;
-                curCol++;
-                curRow--;
-
-                lowRow++;
-            }
-            else if (goUp){
-                goUp = !goUp;
-                goRight = !goRight;
-                curRow++;
-                curCol++;
-
-                upCol--;
-                upRow--;
-                lowCol++;
-            }
+            up++;down--;left++;right--;
+            i = up;
+            j = left;
         }
-
-        return out;
+        return res;
     }
 }
