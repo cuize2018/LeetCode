194c66f1b6672c133f249217cd54f38c78c84eb9
Boom <37618018+cuize2018@users.noreply.github.com>
2020-08-21 10:44:36 +0800

    二叉树的最小深度

--- src/leet/Solution111.java
+++ src/leet/Solution111.java
@@ -1,5 +1,7 @@
 package leet;
 
+import org.omg.CORBA.INTERNAL;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -7,37 +9,35 @@ public class Solution111 {
     public static void main(String[] args) {
 
     }
-
+    
     public static int minDepth(TreeNode root) {
-        if (root == null) return 0;
-        if (root.left == null && root.right == null) return 1;
+        if (root == null)return 0;
 
         int left = minDepth(root.left);
         int right = minDepth(root.right);
 
-        if (left != 0 && right != 0)return Math.min(left, right)+1;
-        if (left == 0)return right + 1;
-        return left + 1;
+        if (left != 0 && right != 0)return  Math.min(left,right) + 1;
+        return  Math.max(left,right) + 1;
     }
 
-    public static int minDepth2(TreeNode root) {
-        if (root == null) return 0;
-        if (root.left == null && root.right == null) return 1;
-
+    public static int minDept2(TreeNode root) {
+        if (root == null)return 0;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
-        int res = 0;
+        int height = 1;
         while (!queue.isEmpty()){
             int size = queue.size();
-            res++;
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.remove();
-                if (node.left== null && node.right == null)return res;
+                if (node.left == null && node.right == null){
+                    return height;
+                }
 
                 if (node.left != null)queue.add(node.left);
                 if (node.right != null)queue.add(node.right);
             }
+            height++;
         }
-        return res;
+        return height;
     }
 }
