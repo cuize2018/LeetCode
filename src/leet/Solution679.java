91aca5928cd7dae996b3cbfb09ed57073da08476
Boom <37618018+cuize2018@users.noreply.github.com>
2020-08-22 10:40:34 +0800

    24点

--- src/leet/Solution679.java
+++ src/leet/Solution679.java
@@ -22,41 +22,47 @@ public class Solution679 {
      * @param nums
      * @return
      */
-    public boolean judgePoint24(int[] nums) {
+    public static boolean judgePoint24(int[] nums) {
         List<Double> list = new ArrayList<>();
         for (int num : nums) {
-            list.add((double) num);
+            list.add((double)num);
         }
-        return dfs2(list);
+
+        return dfs(list);
     }
 
-    public boolean dfs2(List<Double> nums) {
-        if (nums.size() == 0) return false;
-        if (nums.size() == 1) return Math.abs(nums.get(0) - 24) < 1e-6;
+    private static boolean dfs(List<Double> nums) {
+        if (nums.size() == 0)return false;
+        if (nums.size() == 1){
+            return Math.abs(nums.get(0) - 24) < 1e-6;
+        }
 
         for (int i = 0; i < nums.size(); i++) {
             for (int j = 0; j < nums.size(); j++) {
-                if (i != j) {
-                    List<Double> nums2 = new ArrayList<>();
+                if (i != j){
+                    List<Double> temp = new ArrayList<>();
                     for (int k = 0; k < nums.size(); k++) {
-                        if (k != i && k != j) nums2.add(nums.get(k));
+                        if (k != i && k != j){
+                            temp.add(nums.get(k));
+                        }
                     }
 
                     for (int k = 0; k < 4; k++) {
                         // k < 2的运算都符合交换律，无需重复计算
-                        if (k < 2 && j > i) continue;
-                        if (k == 0) nums2.add(nums.get(i) + nums.get(j));
-                        if (k == 1) nums2.add(nums.get(i) * nums.get(j));
-                        if (k == 2) nums2.add(nums.get(i) - nums.get(j));
-                        if (k == 3) {
+                        if (k < 2 && i > j)continue;
+                        if (k == 0)temp.add(nums.get(i) + nums.get(j));
+                        if (k == 1)temp.add(nums.get(i) * nums.get(j));
+                        if (k == 2)temp.add(nums.get(i) - nums.get(j));
+                        if (k == 3){
                             if (nums.get(j) != 0) {
-                                nums2.add(nums.get(i) / nums.get(j));
+                                temp.add(nums.get(i) / nums.get(j));
                             } else {
                                 continue;
                             }
                         }
-                        if (dfs2(nums2)) return true;
-                        nums2.remove(nums2.size() - 1);
+                        boolean f = dfs(temp);
+                        if (f)return true;
+                        temp.remove(temp.size()-1);
                     }
                 }
             }
