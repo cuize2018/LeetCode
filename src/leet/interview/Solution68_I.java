b3ed66bc4da5cfa85d0fe5b714b08321e91216b0
Boom <37618018+cuize2018@users.noreply.github.com>
2020-08-22 13:11:21 +0800

    剑指 Offer 68 - I. 二叉搜索树的最近公共祖先

--- src/leet/interview/Solution68_I.java
+++ src/leet/interview/Solution68_I.java
@@ -17,4 +17,23 @@ public class Solution68_I {
         }
         return root;
     }
+
+    public static TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (p == null && q == null)return root;
+        if (p == null)return q;
+        if (q == null)return p;
+        if (p.val > q.val)return lowestCommonAncestor2(root,q,p);
+
+        if (p.val < root.val && q.val > root.val){
+            return root;
+        }
+
+        if (q.val < root.val){
+            return lowestCommonAncestor2(root.left, p,q);
+        }
+        else if (p.val > root.val){
+            return lowestCommonAncestor2(root.right,p,q);
+        }
+        return root;
+    }
 }
