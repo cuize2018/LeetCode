bba416e76f8374929929674e84ce2742b3383f65
Boom <37618018+cuize2018@users.noreply.github.com>
2020-09-11 10:22:58 +0800

    组合总和 III

--- src/leet/Solution216.java
+++ src/leet/Solution216.java
@@ -1,42 +1,43 @@
 package leet;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Solution216 {
     private List<List<Integer>> out = new ArrayList<>();
-    private Stack<Integer> one = new Stack<>();
+    private Deque<Integer> one = new LinkedList<>();
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int k = 1;
         int n = 7;
         Solution216 s = new Solution216();
-        System.out.println(s.combinationSum3(k,n));
+        System.out.println(s.combinationSum3(k, n));
     }
 
     public List<List<Integer>> combinationSum3(int k, int n) {
-        for (int i = 1;i <= 9;i++){
-            if (k > 0 && n > 0) {
-                one.add(i);
-                helper(k - 1, n - i, i);
-                one.pop();
-            }
-        }
+        if (k == 0) return new ArrayList<>();
+        boolean[] visited = new boolean[10];
+        dfs(k, n, visited, 1);
         return out;
     }
 
-    public void helper(int k, int n, int start_num){
-        if (k == 0 && n == 0){
+    private void dfs(int k, int n, boolean[] visited, int start) {
+        if (k == 0 && n == 0) {
             out.add(new ArrayList<>(one));
             return;
         }
+        //if (k <= 0 || n <= 0) return;
 
-        if (k > 0 && n > 0){
-            for (int i = start_num+1;i <= 9;i++){
-                one.add(i);
-                helper(k-1, n-i, i);
-                one.pop();
+        for (int i = start; i <= 9; i++) {
+            if (!visited[i]) {
+                if (n - i < 0)break;
+
+                one.addLast(i);
+                visited[i] = true;
+
+                dfs(k - 1, n - i, visited, i + 1);
+
+                visited[i] = false;
+                one.removeLast();
             }
         }
     }
